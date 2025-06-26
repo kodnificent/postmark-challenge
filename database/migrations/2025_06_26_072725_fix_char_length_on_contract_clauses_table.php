fcb416fd095d15f0a0c1cda6dccc28dda3809222
kodnificent <mbamaravictor@gmail.com>
2025-06-26 08:55:31 +0100

    convert text altering migration to raw statement

--- database/migrations/2025_06_26_072725_fix_char_length_on_contract_clauses_table.php
+++ database/migrations/2025_06_26_072725_fix_char_length_on_contract_clauses_table.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('contract_clauses', function (Blueprint $table) {
-            $table->text('comment')->nullable()->change();
-        });
+        DB::statement('ALTER TABLE contract_clauses MODIFY comment TEXT NULL;');
     }
 };
